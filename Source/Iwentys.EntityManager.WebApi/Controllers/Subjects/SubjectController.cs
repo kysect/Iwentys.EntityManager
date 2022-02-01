6d3b90464f28d1c6575cbf1ca7ea39ba68f7e73b
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-02-01 19:21:27 +0300

    fix: NotFound result implemented && return type return type generic param fixed

--- Source/Iwentys.EntityManager.WebApi/Controllers/Subjects/SubjectController.cs
+++ Source/Iwentys.EntityManager.WebApi/Controllers/Subjects/SubjectController.cs
@@ -17,25 +17,30 @@ public class SubjectController : ControllerBase
     }
 
     [HttpGet(nameof(Get))]
-    public async Task<ActionResult<List<SubjectProfileDto>>> Get()
+    public async Task<ActionResult<IReadOnlyCollection<SubjectProfileDto>>> Get()
     {
         var response = await _mediator.Send(new GetSubjects.Query());
         return Ok(response.Subjects);
     }
 
     [HttpGet(nameof(SearchSubjects))]
-    public async Task<ActionResult<List<SubjectProfileDto>>> SearchSubjects(int? courseId, StudySemester? semester, int? skip, int? take)
+    public async Task<ActionResult<IReadOnlyCollection<SubjectProfileDto>>> SearchSubjects(
+        int? courseId, StudySemester? semester, int? skip, int? take)
     {
-        var studySearchParameters = new SubjectSearchParametersDto(null, null, null, courseId, semester, skip, take);
+        SubjectSearchParametersDto studySearchParameters
+            = new (null, null, null, courseId, semester, skip, take);
+
         SearchSubjects.Response response = await _mediator.Send(new SearchSubjects.Query(studySearchParameters));
 
         return Ok(response.Subjects);
     }
 
     [HttpGet(nameof(GetSubjectByStudentId))]
-    public async Task<ActionResult<List<SubjectProfileDto>>> GetSubjectByStudentId(int studentId)
+    public async Task<ActionResult<IReadOnlyCollection<SubjectProfileDto>>> GetSubjectByStudentId(int studentId)
     {
-        GetSubjectsByStudentId.Response response = await _mediator.Send(new GetSubjectsByStudentId.Query(studentId));
+        GetSubjectsByStudentId.Response response
+            = await _mediator.Send(new GetSubjectsByStudentId.Query(studentId));
+
         return Ok(response.Subjects);
     }
 
@@ -43,11 +48,14 @@ public class SubjectController : ControllerBase
     public async Task<ActionResult<SubjectProfileDto>> GetSubjectById(int subjectId)
     {
         GetSubjectById.Response response = await _mediator.Send(new GetSubjectById.Query(subjectId));
+
+        if (response?.Subject is null) return NotFound();
+
         return Ok(response.Subject);
     }
 
     [HttpGet(nameof(GetSubjectsByGroupId))]
-    public async Task<ActionResult<List<SubjectProfileDto>>> GetSubjectsByGroupId(int groupId)
+    public async Task<ActionResult<IReadOnlyCollection<SubjectProfileDto>>> GetSubjectsByGroupId(int groupId)
     {
         GetSubjectsByGroupId.Response response = await _mediator.Send(new GetSubjectsByGroupId.Query(groupId));
         return Ok(response.Subjects);
