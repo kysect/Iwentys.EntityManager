0d0965a3a5badd8349e590d3fbc82a8cd203c719
ronimizy <ronimizy@niuitmo.ru>
2022-02-15 23:20:51 +0300

    refactor: added IwentysUser constructor

--- Source/Iwentys.EntityManager.Domain/Users/IwentysUser.cs
+++ Source/Iwentys.EntityManager.Domain/Users/IwentysUser.cs
@@ -2,6 +2,29 @@
 
 public class IwentysUser : UniversitySystemUser
 {
+    public IwentysUser(
+        string firstName,
+        string middleName,
+        string lastName,
+        bool isAdmin,
+        string githubUsername,
+        DateTime creationTime,
+        DateTime lastOnlineTime,
+        string avatarUrl)
+        : base(firstName, middleName, lastName)
+    {
+        ArgumentNullException.ThrowIfNull(githubUsername);
+        ArgumentNullException.ThrowIfNull(avatarUrl);
+
+        IsAdmin = isAdmin;
+        GithubUsername = githubUsername;
+        CreationTime = creationTime;
+        LastOnlineTime = lastOnlineTime;
+        AvatarUrl = avatarUrl;
+    }
+
+    protected IwentysUser() { }
+
     public bool IsAdmin { get; set; }
     public string GithubUsername { get; set; }
     public DateTime CreationTime { get; init; }
