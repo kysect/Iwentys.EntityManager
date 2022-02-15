286adb1cc6392bdd15608f4c599225854357b054
ronimizy <ronimizy@niuitmo.ru>
2022-02-15 23:49:28 +0300

    refactor: added null-checking into IwentysUser

--- Source/Iwentys.EntityManager.Domain/Users/IwentysUser.cs
+++ Source/Iwentys.EntityManager.Domain/Users/IwentysUser.cs
@@ -26,13 +26,14 @@ public class IwentysUser : UniversitySystemUser
     protected IwentysUser() { }
 
     public bool IsAdmin { get; set; }
-    public string GithubUsername { get; set; }
+    public string GithubUsername { get; protected set; }
     public DateTime CreationTime { get; init; }
     public DateTime LastOnlineTime { get; set; }
     public string AvatarUrl { get; set; }
 
     public void UpdateGithubUsername(string githubUsername)
     {
+        ArgumentNullException.ThrowIfNull(githubUsername);
         GithubUsername = githubUsername;
     }
 }
