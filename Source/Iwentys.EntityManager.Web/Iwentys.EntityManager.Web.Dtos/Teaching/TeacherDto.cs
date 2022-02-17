6b91aefb4b654b4dda4b791f68bd56f434d31f96
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-02-18 02:24:27 +0300

    fix: missing dto updated

--- Source/Iwentys.EntityManager.Web/Iwentys.EntityManager.Web.Dtos/Teaching/TeacherDto.cs
+++ Source/Iwentys.EntityManager.Web/Iwentys.EntityManager.Web.Dtos/Teaching/TeacherDto.cs
@@ -2,24 +2,9 @@
 
 namespace Iwentys.EntityManager.WebApiDtos;
 
-public record TeacherDto
-{
-    public int Id { get; init; }
-    public TeacherType TeacherType { get; init; }
-    public string FirstName { get; init; }
-    public string MiddleName { get; init; }
-    public string SecondName { get; init; }
-
-    public TeacherDto()
-    {
-    }
-
-    public TeacherDto(int id, TeacherType teacherType, string firstName, string middleName, string secondName)
-    {
-        TeacherType = teacherType;
-        Id = id;
-        FirstName = firstName;
-        MiddleName = middleName;
-        SecondName = secondName;
-    }
-};
+public record TeacherDto(
+    int Id,
+    TeacherType TeacherType,
+    string FirstName,
+    string MiddleName,
+    string SecondName);
