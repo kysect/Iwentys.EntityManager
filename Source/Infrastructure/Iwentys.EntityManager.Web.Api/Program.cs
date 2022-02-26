67a4fe7b5af91677cd9a3c9c96665a877efc9f56
Fredi Kats <fredikats@outlook.com>
2022-02-26 16:34:34 +0300

    feat: add method for controller dependency registration

--- Source/Infrastructure/Iwentys.EntityManager.Web.Api/Program.cs
+++ Source/Infrastructure/Iwentys.EntityManager.Web.Api/Program.cs
@@ -3,7 +3,7 @@ using Iwentys.EntityManager.Application;
 using Iwentys.EntityManager.Application.Abstractions;
 using Iwentys.EntityManager.DataAccess;
 using Iwentys.EntityManager.DataSeeding;
-using Iwentys.EntityManager.WebApi;
+using Iwentys.EntityManager.Web.Controllers;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddControllers()
-    .AddApplicationPart(typeof(GroupSubjectController).Assembly)
+    .AddEntityManagerControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Source/Presentation/Iwentys.EntityManager.Web.Controllers/ControllerRegistrator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Iwentys.EntityManager.Web.Controllers;
+
+public static class ControllerDependencyInjector
+{
+    public static IMvcBuilder AddEntityManagerControllers(this IMvcBuilder builder)
+    {
+        return builder
+            .AddApplicationPart(typeof(ControllerDependencyInjector).Assembly);
+    }
+}
