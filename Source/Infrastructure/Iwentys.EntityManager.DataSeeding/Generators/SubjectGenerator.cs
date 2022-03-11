ff2ee19426cd5bcafb08fc9b57d1634e92c76a1c
Ar4eR-ValerA <ar4er.valera@gmail.com>
2022-03-11 15:48:30 +0300

    chore: finally we've got exception

---
+++ Source/Infrastructure/Iwentys.EntityManager.DataSeeding/Generators/SubjectGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using Iwentys.EntityManager.DataAccess;
+using Iwentys.EntityManager.Domain;
+
+namespace Iwentys.EntityManager.DataSeeding;
+
+public class SubjectGenerator : IDbContextSeeder
+{
+    private const int SubjectCount = 10;
+    
+    public SubjectGenerator(Faker<Subject> subjectFaker)
+    {
+        Subjects = subjectFaker.Generate(SubjectCount).ToArray();
+        Console.WriteLine();
+    }
+    
+    public Subject[] Subjects { get; }
+    
+    public void Seed(IwentysEntityManagerDbContext context)
+    {
+        context.Subjects.AddRange(Subjects);
+    }
+}
